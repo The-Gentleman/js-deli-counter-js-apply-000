21239a7fa77a0aafad9cbea5099944148ba5ba93
Zeshan Raja <zraja4402@gmail.com>
2019-10-16 02:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,17 +8,25 @@ takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
 takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
 takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 
+
+
 function nowServing(katzDeliLine) {
-// i need to make a firstPerson variable
+//I need to make a variable for the first person in line
+ 
  if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
-  }
-for (var i = 0; i < katzDeliLine.length; i++) {
+ }
+
+ for (var i = 0; i < katzDeliLine.length; i++) {
+  
   var firstPerson = katzDeliLine[0];
   katzDeliLine.shift();
+ 
   return 'Currently serving ' + firstPerson + '.';
+ }
 }
-}
+
+
 
 function currentLine(line) {
   if (line.length === 0) {
