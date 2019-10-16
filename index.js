47dc17611dffa4fa259fc4357fbf2dd6727272a2
Zeshan Raja <zraja4402@gmail.com>
2019-10-16 02:08:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ katzDeliLine.push(newPerson);
 return 'Welcome, ' + newPerson + '. You are number ' + katzDeliLine.length + ' in line.';
 }
 
-takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
-takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
-takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
+// takeANumber(katzDeliLine, "Ada"); // "Welcome, Ada. You are number 1 in line."
+// takeANumber(katzDeliLine, "Grace"); // "Welcome, Grace. You are number 2 in line."
+// takeANumber(katzDeliLine, "Kent"); // "Welcome, Kent. You are number 3 in line."
 
 
 
